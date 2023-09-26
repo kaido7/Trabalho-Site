591a164a643d938dc40e67528d2beabad65ed75d
Gabriel <gabrielsribeiro9999@gmail.com>
2023-09-26 20:31:17 -0300

    trabalho

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+// 
+
+// const box = document.querySelector(".conteiner-carrosel");
+// const imagens = document.querySelectorAll(".conteiner-carrosel ul a");
+
+// let contador = 0;
+
+// function slider() {
+//   contador++;
+
+//   if (contador > imagens.length - 1) {
+//     contador = 0;
+//   }
+
+//   box.style.transform = `translateX(${-contador * 100}%)`;
+// }
+
+// setInterval(slider, 2000);
